294264270fe90936831389186fd9cbe2423afab0
roian6 <roian6@naver.com>
2020-02-10 19:23:02 +0900

    ADD splash screen ADD fonts, colors

---
+++ app/src/main/java/com/example/starterpack/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.starterpack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //fullscreen
+
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 1500); //wait 1.5 sec
+    }
+}
